16c66a6fa739157b69e96793de11e22711d88d1d
George Gastaldi <gegastaldi@gmail.com>
2016-09-26 18:30:19 -0300

    Fixed Error: ngModel:numfmt Model is not of type number

--- web/src/main/webapp/js/app.js
+++ web/src/main/webapp/js/app.js
@@ -29,4 +29,18 @@ angular.module('jboss-forge', [ 'ui.router', 'treeControl']).run(
 				templateUrl : 'views/wizard.html',
 				controller : 'wizardCtrl'
 			});
-		});
+		})
+
+.directive('stringToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        return '' + value;
+      });
+      ngModel.$formatters.push(function(value) {
+        return parseFloat(value);
+      });
+    }
+  };
+});
